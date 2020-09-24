2eb802e45d76f4b864a84a979396733fd8a305a4
Jeff <j.vogelesang@gmail.com>
2020-09-24 10:29:24 +0200

    src verplaatst

--- src/Tafels.java
+++ src/Tafels.java
@@ -1,5 +1,28 @@
 public class Tafels {
+    public Tafels() {
+    }
+
     public static void main(String[] args) {
-          System.out.println();
+        int x = 8;
+        int a = x * 1;
+        int b = x * 2;
+        int c = x * 3;
+        int d = x * 4;
+        int e = x * 5;
+        int f = x * 6;
+        int g = x * 7;
+        int h = x * 8;
+        int i = x * 9;
+        int j = x * 10;
+        System.out.println(" 1x8=" + a);
+        System.out.println(" 2x8=" + b);
+        System.out.println(" 3x8=" + c);
+        System.out.println(" 4x8=" + d);
+        System.out.println(" 5x8=" + e);
+        System.out.println(" 6x8=" + f);
+        System.out.println(" 7x8=" + g);
+        System.out.println(" 8x8=" + h);
+        System.out.println(" 9x8=" + i);
+        System.out.println("10x8=" + j);
     }
 }
