1069a40b688fcf0c2e469fb9100ab50ad15aa498
Jeff <j.vogelesang@gmail.com>
2020-09-24 18:28:38 +0200

    Cijfer invoer + bijbehorende tafel

--- src/Tafels.java
+++ src/Tafels.java
@@ -6,36 +6,50 @@ public class Tafels {
 
     public static void main(String[] args) {
 
-        String y = "voer getal in"; //opdrachtregel
-        System.out.println(y);
 
-        var s = new Scanner(System.in); //tellen input cijfers
-        var getal = s.nextInt(); //geef input in cijfers
+            String y = "voer getal in"; //opdrachtregel
+            System.out.println(y);
 
-        String z = "De tafel van " +getal +" is:"; //antwoordregel
-        System.out.println(z);
+            var s = new Scanner(System.in); //tellen input cijfers
+            var getal = s.nextInt(); //geef input in cijfers
 
-        int x = getal;
-        int a = x * 1;
-        int b = x * 2;
-        int c = x * 3;
-        int d = x * 4;
-        int e = x * 5;
-        int f = x * 6;
-        int g = x * 7;
-        int h = x * 8;
-        int i = x * 9;
-        int j = x * 10;
-        System.out.println(" 1x" + x + "=" + a);
-        System.out.println(" 2x" + x + "=" + b);
-        System.out.println(" 3x" + x + "=" + c);
-        System.out.println(" 4x" + x + "=" + d);
-        System.out.println(" 5x" + x + "=" + e);
-        System.out.println(" 6x" + x + "=" + f);
-        System.out.println(" 7x" + x + "=" + g);
-        System.out.println(" 8x" + x + "=" + h);
-        System.out.println(" 9x" + x + "=" + i);
-        System.out.println("10x" + x + "=" + j);
+            String z = "De tafel van " + getal + " is:"; //antwoordregel
+            System.out.println(z);
+
+            // tafel zonder loop
+//        int x = getal;
+//        int a = x * 1;
+//        int b = x * 2;
+//        int c = x * 3;
+//        int d = x * 4;
+//        int e = x * 5;
+//        int f = x * 6;
+//        int g = x * 7;
+//        int h = x * 8;
+//        int i = x * 9;
+//        int j = x * 10;
+//        System.out.println(" 1x" + x + "=" + a);
+//        System.out.println(" 2x" + x + "=" + b);
+//        System.out.println(" 3x" + x + "=" + c);
+//        System.out.println(" 4x" + x + "=" + d);
+//        System.out.println(" 5x" + x + "=" + e);
+//        System.out.println(" 6x" + x + "=" + f);
+//        System.out.println(" 7x" + x + "=" + g);
+//        System.out.println(" 8x" + x + "=" + h);
+//        System.out.println(" 9x" + x + "=" + i);
+//        System.out.println("10x" + x + "=" + j);
+
+            // tafel met loop
+
+            for (int i = 1; i <= 10; i++) {
+
+                System.out.println(i + "x" + getal + "=" + getal * i);
+            }
+        //afronden van decimalen
+//        double d = 10.0023234;
+//                var i = (int) d; //afronden met decimalen kan met (int)
+//        System.out.println(i);
+
 
     }
 }
